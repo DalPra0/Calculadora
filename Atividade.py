b87ef1b5b42675e70ab83933fc8543643029c2fa
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2024-03-21 15:43:09 -0300

    Nova versao do codigo

--- Atividade.py
+++ Atividade.py
@@ -2,33 +2,36 @@ print("")
 print("Lucas Dal Pra Brascher - 1C - Atividade 1: Calculadora no GitHub")
 print("")
 
-num1 = int(input("Digite o primeiro numero: "))
-num2 = int(input("Digite o segundo numero: "))
+while True:
+    num1 = int(input("Digite o primeiro numero: "))
+    num2 = int(input("Digite o segundo numero: "))
 
-print("1 - Soma ; 2 -  Subtração ; 3 - Multiplicação ; 4 - Divisão ; 5 - Sair")
+    print("1 - Soma ; 2 -  Subtração ; 3 - Multiplicação ; 4 - Divisão ; 5 - Sair")
 
-oper = int(input("Seguindo o exemplo acima, qual operação voce deseja realizar? "))
+    oper = int(input("Seguindo o exemplo acima, qual operação voce deseja realizar? "))
 
-if oper == 1:
+    if oper == 1:
+        resultado = num1 + num2
+        print("A operação foi soma: ", num1, "+", num2, "=", resultado)
 
-    resultado = num1+num2
-    print("A operação foi soma: ", num1,"+", num2, "=", resultado)
+    elif oper == 2:
+        resultado = num1 - num2
+        print("A operação foi subtração: ", num1, "-", num2, "=", resultado)
 
-elif oper == 2:
+    elif oper == 3:
+        resultado = num1 * num2
+        print("A operação foi multiplicação: ", num1, "x", num2, "=", resultado)
 
-    resultado = num1-num2
-    print("A operação foi subtração: ", num1, "-", num2, "=", resultado)
+    elif oper == 4:
+        if num2 != 0:
+            resultado = num1 / num2
+            print("A operação foi divisão: ", num1, "/", num2, "=", resultado)
+        else:
+            print("Erro: Divisão por zero!")
 
-elif oper == 3:
+    elif oper == 5:
+        print("Encerrando o programa...")
+        break
 
-    resultado = num1*num2
-    print("A operação foi multiplicação: ", num1, "x", num2, "=", resultado)
-
-elif oper == 4:
-
-    resultado = num1/num2
-    print("A operação foi divisão: ", num1, "%", num2, "=", resultado)
-
-elif oper == 5:
-    
-    exit
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
